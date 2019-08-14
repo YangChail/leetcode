6364c740f0330b4a261715e2cff8b8a7434be797
yangc <yangc@mchz.com.cn>
2019-08-14 16:07:28 +0800

    237. 删除链表中的节点 请编写一个函数，

---
+++ src/main/java/leetcode/utils/ListNode.java
@@ -0,0 +1,29 @@
+package leetcode.utils;
+
+public class ListNode {
+	public int val;
+	public ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+
+	// 数组转换成链表
+	public static ListNode arrayToListNode(int[] s) {
+		ListNode root = new ListNode(s[0]);// 生成链表的根节点，并将数组的第一个元素的值赋给链表的根节点
+		ListNode other = root;// 生成另一个节点，并让other指向root节点，other在此作为一个临时变量，相当于指针
+		for (int i = 1; i < s.length; i++) {// 由于已给root赋值，所以i从1开始
+			ListNode temp = new ListNode(s[i]);// 每循环一次生成一个新的节点,并给当前节点赋值
+			other.next = temp;// 将other的下一个节点指向生成的新的节点
+			other = temp;// 将other指向最后一个节点(other的下一个节点) other=other.getNext();
+		}
+		return root;
+	}
+
+	public static void print(ListNode listNode) {
+		while (listNode.next != null) {
+			System.out.println(listNode.val);
+			listNode = listNode.next;
+		}
+	}
+}
